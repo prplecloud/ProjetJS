2127e27bd9ccc04936a4eaaa439dfb1118caa76f
Enzo CARBONARA <enzo.carbonara@ynov.com>
2024-06-16 12:14:24 +0200

    Finished

--- frontend/assets/sql/4images.sql
+++ frontend/assets/sql/4images.sql
@@ -18,7 +18,7 @@ INSERT INTO `products`(`name`, `price`, `stock`, `image_url`, `image_url2`, `ima
 VALUES 
 ("Pokémon - Boite de Boosters Français - Display 36 Boosters EV06 - Ecarlate et Violet - Mascarade Crépusculaire
 ", 169.99, 3, 
-"assets/img/articles/Pokemon/Blister/Français/Mascarade.jpg", "assets/img/articles/Pokemon/booster/Français/Mascarade2.jpg", "assets/img/articles/Pokemon/booster/Français/Mascarade3.jpg", "assets/img/articles/Pokemon/booster/Français/Mascarade4.jpg",
+"../assets/img/articles/Pokemon/Blister/Français/Mascarade.jpg", "../assets/img/articles/Pokemon/booster/Français/Mascarade2.jpg", "../assets/img/articles/Pokemon/booster/Français/Mascarade3.jpg", "../assets/img/articles/Pokemon/booster/Français/Mascarade4.jpg",
 "Scellé en Boîte de 36 Boosters.
 Bienvenue sur les terres de Septentria, un lieu où les humains et les Pokémon vivent en harmonie avec la nature. Les légendes sont légion, mais la vérité n'est pas toujours là où on l'attend... Découvrez les mystères du Pokémon légendaire masqué, Ogerpon, qui apparaît en tant que Pokémon-ex Téracristal sous quatre types terrifiants, et faites équipe avec des Pokémon nouvellement découverts, tels qu'Ursaking Lune Vermeille-ex et Théffroyable-ex. Gagnant en puissance, Amphinobi, Lanssorien et Volcaropod nous éblouissent en tant que Pokémon-ex Téracristal, et davantage de cartes HIGH-TECH se joignent à la fête dans l'extension Écarlate et Violet - Mascarade Crépusculaire du JCC Pokémon.
 Chaque Display contient 36 boosters de 10 cartes en Français de l'extension Pokémon Écarlate et Violet - Mascarade Crépusculaire
@@ -26,7 +26,7 @@ VERSION FRANÇAISE",
 10, 16, 1, 1, 1, 3),
 
 ("Pokémon - Booster Français - EV05 - Ecarlate et Violet - Forces Temporelles", 5.30, 10, 
-"assets/img/articles/Pokemon/booster/Français/forces-temporelles1FR.jpg", "assets/img/articles/Pokemon/booster/Français/forces-temporelles1FR-2.jpg", "assets/img/articles/Pokemon/booster/Français/forces-temporelles1FR-3.jpg", "assets/img/articles/Pokemon/booster/Français/forces-temporelles1FR-4.jpg", 
+"../assets/img/articles/Pokemon/booster/Français/forces-temporelles1FR.jpg", "../assets/img/articles/Pokemon/booster/Français/forces-temporelles1FR-2.jpg", "../assets/img/articles/Pokemon/booster/Français/forces-temporelles1FR-3.jpg", "../assets/img/articles/Pokemon/booster/Français/forces-temporelles1FR-4.jpg", 
 "Booster de 10 cartes.
 Les rangs des Pokémon Temps passé et Temps futur continuent de grossir. Serpente-Eau-ex se libère des chaînes du passé aux côtés d’Ire-Foudre-ex, tandis que Vert-de-Fer-ex impose une justice high-tech avec Chef-de-Fer-ex. À l’extérieur de la Zone Zéro, Triopikeau et Farigiraf changent de type en tant que Pokémon-ex Téracristal, et les Dresseurs et Dresseuses Pokémon du monde entier se préparent pour le retour des cartes HIGH-TECH aux effets puissants et uniques. Une brèche temporelle s’est ouverte, amenant avec elle des bêtes sauvages et des créatures futuristes dans l’extension Écarlate et Violet – Forces Temporelles du JCC Pokémon !
 Plus de 160 cartes :
@@ -44,12 +44,12 @@ VERSION FRANÇAISE" , 0, 9, 1, 1, 1, 1),
 
 
 ("Pokémon - Booster Français - EV4.5 - Destinées De Paldéa", 8.90, 10, 
-"assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa.jpg", "assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-2.jpg", "assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-3.jpg", "assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-4.jpg",
+"../assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa.jpg", "../assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-2.jpg", "../assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-3.jpg", "../assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-4.jpg",
 "La lumière des projecteurs brille sur les Pokémon chromatiques alors qu’ils prennent leur destinée en main et font leur grand retour dans le JCC Pokémon. Rayonnez de splendeur et découvrez des merveilles étincelantes dans l’extension Écarlate et Violet – Destinées de Paldea.
 VERSION FRANÇAISE" , 0, 10, 1, 1, 1, 1),
 
 ("Pokémon - Booster Français - EV04 - Ecarlate et Violet - Faille Paradoxe", 5.90, 10, 
-"assets/img/articles/Pokemon/booster/Français/faille-paradoxe.jpg", "assets/img/articles/Pokemon/booster/Français/faille-paradoxe-2.jpg","assets/img/articles/Pokemon/booster/Français/faille-paradoxe-3.jpg","assets/img/articles/Pokemon/booster/Français/faille-paradoxe-4.jpg",
+"../assets/img/articles/Pokemon/booster/Français/faille-paradoxe.jpg", "../assets/img/articles/Pokemon/booster/Français/faille-paradoxe-2.jpg","../assets/img/articles/Pokemon/booster/Français/faille-paradoxe-3.jpg","../assets/img/articles/Pokemon/booster/Français/faille-paradoxe-4.jpg",
 "Booster de 10 cartes.
 Plongez dans les nuages et explorez des terres qui semblent intemporelles ! Avec leurs attaques féroces, les Pokémon Temps passé tels que Rugit-Lune-ex et Pelage-Sablé-ex apparaissent aux côtés de Pokémon Temps futur artificiels comme Gardede- Fer-ex et Paume-de-Fer-ex. Carchacrok-ex, Mewtwo-ex et d’autres Pokémon ne sont pas en reste, et se téracristallisent pour obtenir de nouveaux types. Quant à Carmadura-ex et Gromago-ex, ils rejoignent d’autres Pokémon-ex dans la bataille ! L’aventure vous attend, alors que les frontières temporelles s’entrechoquent dans Écarlate et Violet – Faille Paradoxe du JCC Pokémon.
 Contenu
@@ -61,19 +61,19 @@ Pokémon-ex Téracristal dont le type a changé avec une apparence rappelant les
 VERSION FRANÇAISE" , 0, 12, 1, 1, 1, 1),
 
 ("Pokémon - Booster Français - EV01 - Ecarlate et Violet", 5.90, 10, 
-"assets/img/articles/Pokemon/booster/Français/ecarlate-et-violet.jpg", "assets/img/articles/Pokemon/booster/Français/ecarlate-et-violet-2.jpg", "assets/img/articles/Pokemon/booster/Français/ecarlate-et-violet-3.jpg", "assets/img/articles/Pokemon/booster/Français/ecarlate-et-violet-4.jpg",
+"../assets/img/articles/Pokemon/booster/Français/ecarlate-et-violet.jpg", "../assets/img/articles/Pokemon/booster/Français/ecarlate-et-violet-2.jpg", "../assets/img/articles/Pokemon/booster/Français/ecarlate-et-violet-3.jpg", "../assets/img/articles/Pokemon/booster/Français/ecarlate-et-violet-4.jpg",
 "Booster de 10 cartes.
 Partez pour la région de Paldea ! Rencontrez les premiers partenaires Poussacha, Chochodile et Coiffeton, et découvrez le pouvoir des Pokémon légendaires Koraidon et Miraidon en tant que Pokémon-ex. D’autres Pokémon-ex s’engagent au combat, notamment Arcanin et Léviator avec de nouvelles apparences époustouflantes. Encore plus de Pokémon apparaissent tels de vrais chefs-d’œuvre sur des cartes aux illustrations rares. Découvrez-les tous dans l’extension Écarlate et Violet du JCC Pokémon.
 VERSION FRANÇAISE" , 0, 11, 1, 1, 1, 1),
 
 ("Pokémon - Booster Français - EB12 - Epée et Bouclier 12 - Tempête Argentée", 9.90, 10, 
-"assets/img/articles/Pokemon/booster/Français/tempête-argentée.png", "assets/img/articles/Pokemon/booster/Français/tempête-argentée-2.jpg","assets/img/articles/Pokemon/booster/Français/tempête-argentée-3.jpg","assets/img/articles/Pokemon/booster/Français/tempête-argentée-4.jpg",
+"../assets/img/articles/Pokemon/booster/Français/tempête-argentée.png", "../assets/img/articles/Pokemon/booster/Français/tempête-argentée-2.jpg","../assets/img/articles/Pokemon/booster/Français/tempête-argentée-3.jpg","../assets/img/articles/Pokemon/booster/Français/tempête-argentée-4.jpg",
 "Booster de 10 cartes.
 Offrez une Puissance VSTAR à n’importe quel Pokémon-V avec ces cartes Outil Pokémon spéciales. Attachez Pierre Scellée Sylvestre à l’un de vos Pokémon-V pour qu’il obtienne le talent Alchimie Star. Utilisez cette puissance VSTAR pour chercher dans votre deck une carte et l’ajouter à votre main, c’est parfait pour trouver LA carte dont vous avez besoin, quand vous en avez besoin. Si vous préférez l’offensive, vous pouvez attacher Pierre Scellée Terrestre à l’un de vos Pokémon-V pour qu’il obtienne l’attaque Gravité Star, qui place des marqueurs de dégâts sur chacun des Pokémon-V de votre adversaire jusqu’à ce qu’il ne reste à chacun d’eux que 100 PV. Renversez votre adversaire avec ces puissantes cartes Outil Pokémon, et faites de l’un de vos Pokémon une star !
 VERSION FRANÇAISE" , 0, 14, 1, 1, 1, 1),
 
 ("Pokémon - Booster Français - EB09 - Epée et Bouclier 9 - Stars Étincelantes", 11.90, 10, 
-"assets/img/articles/Pokemon/booster/Français/stars-étincelantes.jpg", "assets/img/articles/Pokemon/booster/Français/stars-étincelantes-2.jpg","assets/img/articles/Pokemon/booster/Français/stars-étincelantes-3.jpg","assets/img/articles/Pokemon/booster/Français/stars-étincelantes-4.jpg",
+"../assets/img/articles/Pokemon/booster/Français/stars-étincelantes.jpg", "../assets/img/articles/Pokemon/booster/Français/stars-étincelantes-2.jpg","../assets/img/articles/Pokemon/booster/Français/stars-étincelantes-3.jpg","../assets/img/articles/Pokemon/booster/Français/stars-étincelantes-4.jpg",
 "Booster de 10 cartes.
 Débordant de lumière, Arceus-VSTAR descend des cieux pour partager ses pouvoirs célestes avec d'autres Pokémon-V. Découvrez des constellations scintillantes et commencez votre voyage vers la grandeur stellaire avec l'extension Épée et Bouclier – Stars Étincelantes du JCC Pokémon.
 Plus de 170 cartes
@@ -87,7 +87,7 @@ VERSION FRANÇAISE" , 0, 13, 1, 1, 1, 1),
 
 ("Pokémon - Coffret - EV04 Ecarlate et Violet - Faille Paradoxe - Dresseur d'Elite : Garde-de-Fer
 ", 54.90, 2, 
-"assets/img/articles/Pokemon/coffrets/Garde-de-Fer.jpg", "assets/img/articles/Pokemon/coffrets/Garde-de-Fer2.jpg", "assets/img/articles/Pokemon/coffrets/Garde-de-Fer3.jpg", "assets/img/articles/Pokemon/coffrets/Garde-de-Fer4.jpg",
+"../assets/img/articles/Pokemon/coffrets/Garde-de-Fer.jpg", "../assets/img/articles/Pokemon/coffrets/Garde-de-Fer2.jpg", "../assets/img/articles/Pokemon/coffrets/Garde-de-Fer3.jpg", "../assets/img/articles/Pokemon/coffrets/Garde-de-Fer4.jpg",
 "Plongez dans les nuages et explorez des terres qui semblent intemporelles ! Avec leurs attaques féroces, les Pokémon Temps passé tels que Rugit-Lune-exet Pelage-Sablé-ex apparaissent aux côtés de Pokémon Temps futur artificiels comme Gardede-Fer-ex et Paume-de-Fer-ex. Carchacrok-ex, Mewtwo-ex et d’autres Pokémon ne sont pas en reste, et se téracristallisent pour obtenir de nouveaux types. Quant à Carmadura-ex et Gromago-ex, ils rejoignent d’autres Pokémon-ex dans la bataille ! L’aventure vous attend, alors que les frontières temporelles s’entrechoquent dans Écarlate et Violet –FailleParadoxedu JCC Pokémon.
 Contenu :
 9 boosters Écarlate et Violet – Faille Paradoxe du JCC Pokémon,
@@ -108,14 +108,14 @@ L’introduction dans le JCC Pokémon des Pokémon Paradox, de nouveaux Pokémon
 
 ("Pokémon - The Pokémon Company - Coffret - EV4.5 Ecarlate et Violet - Destinées de Paldea - Bundle Lot de 6 Boosters
 ", 44.90, 2, 
-"assets/img/articles/Pokemon/coffrets/destinée-paldéa-6booster.jpg", "assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa.jpg", "assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-2.jpg", "assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-3.jpg",
+"../assets/img/articles/Pokemon/coffrets/destinée-paldéa-6booster.jpg", "../assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa.jpg", "../assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-2.jpg", "../assets/img/articles/Pokemon/booster/Français/destinée-de-paldéa-3.jpg",
  "Contenu :
 6 boosters Écarlate et Violet – Destinées de Paldea
 Les Pokémon chromatiques sont sous le feu des projecteurs lors de leur retour tant attendu dans le JCC Pokémon. Pikachu chromatique éclaire le chemin tout comme Forgelina, Malvalame, Oyacata, et plus de 100 autres Pokémon chromatiques suivront leur trace. Pendant ce temps, Fort-Ivoire et Roue-de-Fer font leur apparition en tant que Pokémon-ex Temps passé et Temps futur, et Dracaufeu, Foretress et Cléopsytra démontrent leurs compétences uniques en tant que Pokémon-ex Téracristal chromatiques. Faites entrer la lumière et découvrez les merveilles étincelantes de l'extension Écarlate et Violet – Destinées de Paldea.
 " , 0, 10, 1, 1, 1, 4),
 
 ("Pokémon - Pokébox - Mini Tin - EV4.5 Ecarlate et Violet - Destinées de Paldea : Dofin", 19.90, 5, 
-"assets/img/articles/Pokemon/tin-box/DophinTin1FR.jpg", "assets/img/articles/Pokemon/tin-box/FlamignolTin1FR.jpg", "assets/img/articles/Pokemon/tin-box/ForugetteTin1FR.jpg", "assets/img/articles/Pokemon/tin-box/OliviniTin1FR.jpg",
+"../assets/img/articles/Pokemon/tin-box/DophinTin1FR.jpg", "../assets/img/articles/Pokemon/tin-box/FlamignolTin1FR.jpg", "../assets/img/articles/Pokemon/tin-box/FourgetteTin1FR.jpg", "../assets/img/articles/Pokemon/tin-box/OliviniTin1FR.jpg",
 "Elle tient au creux de la main, remplie de promesses : la Mini-boîte est remplie de cartes ! Elle est parfaite pour cette extension spéciale plein de duels, pour tous les Dresseurs et les collectionneurs. Cinq visuels sont disponibles !
 Contenu :
 2 boosters Écarlate et Violet – Destinées de Paldea du JCC Pokémon
